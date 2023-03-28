2fc905ee249153514230ad8fba0628ea7ec516b4
RAJESHWARAN S <rbd.7284@gmail.com>
2023-03-28 11:57:02 +0530

    Dropdown hover effect added, now working on gallery

---
+++ main.js
@@ -0,0 +1,9 @@
+// hover dropdown menu
+$(document).ready(function(){
+    $(".dropdown").hover(function(){
+        var dropdownMenu = $(this).children(".dropdown-menu");
+        if(dropdownMenu.is(":visible")){
+            dropdownMenu.parent().toggleClass("open");
+        }
+    });
+}); 
